a117f3761aecdc3031365ec43bf0e855f0ee40c8
BeefyDonkey <79321338+BeefyDonkey@users.noreply.github.com>
2021-05-11 19:35:32 +0300

    Add files via upload

---
+++ Ammo.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ammo : MonoBehaviour
+{
+    [SerializeField] AmmoSlot[] ammoSlots;
+
+    [System.Serializable]
+    private class AmmoSlot
+    {
+        public AmmoType ammoType;
+        public int ammoAmount;
+        public int ammoInMagazine;
+    }
+
+    public void ReloadGun(AmmoType ammoType, int ammoInMagazine)
+    {
+        var weapon = GetAmmoSlot(ammoType);
+
+        if (weapon.ammoAmount < ammoInMagazine)
+        {
+            weapon.ammoInMagazine = weapon.ammoAmount;
+            weapon.ammoAmount -= weapon.ammoAmount;
+        }
+        else
+        {
+            weapon.ammoAmount -= ammoInMagazine;
+            weapon.ammoInMagazine = ammoInMagazine;
+        }        
+    }
+
+    public void IncreaseAmmo(AmmoType ammoType, int increaseAmount)
+    {
+        GetAmmoSlot(ammoType).ammoAmount += increaseAmount;
+    }
+
+    public void ReduceAmmo(AmmoType ammoType)
+    {
+        GetAmmoSlot(ammoType).ammoInMagazine--;
+    }
+
+    public int GetAmmoInMagazine(AmmoType ammoType)
+    {
+        return GetAmmoSlot(ammoType).ammoInMagazine;
+    }
+
+    public int GetCurrentAmmo(AmmoType ammoType)
+    {
+        return GetAmmoSlot(ammoType).ammoAmount;
+    }
+
+    private AmmoSlot GetAmmoSlot(AmmoType ammoType)
+    {
+        foreach (AmmoSlot slot in ammoSlots)
+        {
+            if (slot.ammoType == ammoType)
+            {
+                return slot;
+            }
+        }
+
+        return null;
+    }
+}
---
+++ AmmoPickup.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmmoPickup : MonoBehaviour
+{
+    [SerializeField] int ammoAmount;
+    [SerializeField] AmmoType ammoType;
+
+    [SerializeField] AudioSource audioSource;
+    [SerializeField] AudioClip ammoCollectSFX;
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            FindObjectOfType<Ammo>().IncreaseAmmo(ammoType, ammoAmount);
+            audioSource.PlayOneShot(ammoCollectSFX);
+            DisableAllMeshes();
+            Destroy(this.gameObject, 0.6f);
+        }
+    }
+
+    private void DisableAllMeshes()
+    {
+        Component[] meshes;
+
+        meshes = GetComponentsInChildren<MeshRenderer>();
+
+        foreach (var mesh in meshes)
+        {
+            mesh.gameObject.GetComponent<MeshRenderer>().enabled = false;
+        }
+    }
+}
